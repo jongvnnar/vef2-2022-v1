5e2a555f534c376ae9b71d3b054294cd1473beae
jongvnnar <jgh060@gmail.com>
2022-01-25 22:26:24 +0000

    test build

--- src/build.js
+++ src/build.js
@@ -1,5 +1,6 @@
-import { mkdir, readdir, stat } from 'fs/promises';
-
+import { mkdir, readdir, stat, writeFile } from 'fs/promises';
+import { makeIndex } from './make_html.js';
+import { join } from 'path';
 const DATA_DIR = './data';
 const OUTPUT_DIR = './dist';
 
@@ -23,7 +24,8 @@ async function main() {
   if (!(await dirExists(OUTPUT_DIR))) {
     await mkdir(OUTPUT_DIR);
   }
-
+  const index = makeIndex();
+  await writeFile(join(OUTPUT_DIR, 'index.html'), index, { flag: 'w+' });
   console.log('whatup');
 }
 
