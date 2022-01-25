69260be00785f4052e023e360e8247c4bfeb0832
jongvnnar <jgh060@gmail.com>
2022-01-25 20:45:24 +0000

    create functions for data analysis

---
+++ src/calc.js
@@ -0,0 +1,52 @@
+/**
+ * Calculate the variance of a dataset
+ * @param {Array<number>} data numerical data
+ * @returns {number} the variance of the dataset
+ */
+export function variance(data) {}
+/**
+ * Calculate the max of a dataset
+ * @param {Array<number>} data numerical data
+ * @returns {number} the max of the dataset
+ */
+export function max(data) {}
+/**
+ * Calculate the mean of a dataset
+ * @param {Array<number>} data numerical data
+ * @returns {number} the mean of the dataset
+ */
+export function mean(data) {}
+/**
+ * Calculate the median of a dataset
+ * @param {Array<number>} data numerical data
+ * @returns {number} the median of the dataset
+ */
+export function median(data) {}
+/**
+ * Calculate the min of a dataset
+ * @param {Array<number>} data numerical data
+ * @returns {number} the min of the dataset
+ */
+export function min(data) {}
+/**
+ * Calculate the stddev of a dataset
+ * @param {Array<number>} data numerical data
+ * @returns {number} the stddev of the dataset
+ */
+export function stddev(data) {}
+/**
+ * Calculate the sum of a dataset
+ * @param {Array<number>} data numerical data
+ * @returns {number} the sum of the dataset
+ */
+export function sum(data) {}
+/**
+ * Calculate the range of a dataset
+ * @param {Array<number>} data numerical data
+ * @returns {number} the range of the dataset
+ */
+export function range(data) {}
+
+export function calculateAnalysis(data) {
+  return {};
+}
